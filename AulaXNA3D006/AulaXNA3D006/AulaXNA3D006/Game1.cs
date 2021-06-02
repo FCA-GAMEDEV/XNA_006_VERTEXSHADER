a2b7a971eea880b793c03653c544c3b52ef44eba
FCA-GAMEDEV <85238682+FCA-GAMEDEV@users.noreply.github.com>
2021-06-02 16:36:54 -0300

    Add files via upload

---
+++ AulaXNA3D006/AulaXNA3D006/AulaXNA3D006/Game1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace AulaXNA3D006
+{
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        _Screen screen;
+        _Camera camera;
+        _Quad quad;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 800;
+            graphics.PreferredBackBufferHeight = 600;
+        }
+
+        protected override void Initialize()
+        {
+            this.screen = _Screen.GetInstance();
+            this.screen.SetWidth(graphics.PreferredBackBufferWidth);
+            this.screen.SetHeight(graphics.PreferredBackBufferHeight);
+
+            this.camera = new _Camera();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            this.quad = new _Quad(GraphicsDevice, this, @"Textures\flagACBF", @"Effects\MotionEffect");
+        }
+
+        protected override void UnloadContent()
+        {
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                this.Exit();
+
+            this.camera.Update(gameTime);
+            this.quad.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            RasterizerState rs = new RasterizerState();
+            rs.CullMode = CullMode.None;
+            //rs.FillMode = FillMode.WireFrame;
+            GraphicsDevice.RasterizerState = rs;
+            
+            this.quad.Draw(this.camera);
+
+            base.Draw(gameTime);
+        }
+    }
+}
